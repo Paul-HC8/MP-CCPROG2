260e4533f2f2b8b5f8cf4695e9b85761c59298cb
Paul Haven Cortez <cortezpaulhaven@gmail.com>
2026-03-03 14:29:19 +0800

    Added menu/inteface to main

--- Phase1.c
+++ Phase1.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <Windows.h>
 
 // Struct declaration for basketball player stats (points, rebounds, assists)
 struct Stats{
@@ -41,7 +44,43 @@ struct GameRecord{
 
 int main()
 {
-    
+    int choice;
 
+    // Prints out the Menu/Interface for our Simulator
+    while (choice != 4) 
+    {
+    	system("cls");
+        printf("=========================================\n");
+        printf("===  ---  VALORANT MASTERS DLSU  ---  ===\n");
+        printf("=========================================\n");
+        printf("[1] THE TIP-OFF (SIMULATE GAME)\n");
+        printf("[2] THE ROAD TO NATIONALS (STANDINGS)\n");
+        printf("[3] TEAM SCOUTING REPORT (AVERAGES)\n");
+        printf("[4] ACE PLAYER DATA (PLAYER STATS)\n");
+        printf("[5] TOURNAMENT SCOREBOOK (HISTORY)\n");
+        printf("[6] MVP RACE CANDIDATES\n");
+        printf("[7] RIVALRY CHECK (HEAD-TO-HEAD)\n");
+        printf("[7] RIVALRY CHECK (HEAD-TO-HEAD)\n");
+        printf("[8] VIEW BOX SCORE\n");
+        printf("[9] LEAVE THE COURT (EXIT)\n");
+        
+        printf("Enter choice: ");
+        
+        scanf("%d", &choice);
+
+        if (choice == 1) 
+        {
+            addUser(users, &userCount); 
+        }
+        else if (choice == 2) 
+        {
+            authenticate(users, userCount); 
+        }
+        else if (choice == 3) 
+        {
+            updateUser(users, userCount); 
+        }
+        system("pause");
+    }
     return 0;
 }
